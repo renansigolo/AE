1acce6dc467b9f75187eb99d58fe9d55620e0be4
Renan Sigolo <renan.sigolo@gmail.com>
2021-07-30 19:55:23 +1000

    style: adjust about page

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -6,7 +6,7 @@ get_header();
 
 ?>
 <div class="container pt-5 page-about-container">
-	<div class="col-12 offset-md-2 col-md-8 about-info-1">
+	<div class="col-12 col-md-9 mx-auto about-info-1">
 		<div class="ratio ratio-16x9">
 			<?php echo get_field( 'about_info_1_video_link' ); ?>
 		</div>
@@ -17,31 +17,33 @@ get_header();
 		</div>
 	</div>
 
-	<div class="col-12 offset-md-2 col-md-8 about-info-2">
+	<div class="col-12 col-md-9 mx-auto about-info-2">
 		<div class="row">
 			<div class="col-12 banner-header-hashtag">
-				<h1><?php echo get_field( 'about_info_2_banner_title' ); ?></h1>
-				<h4><?php echo get_field( 'about_info_2_hashtag' ); ?></h4>
+			<?php if ( get_field( 'about_info_2_banner_title' )) : ?>	
+				<h1><?php the_field( 'about_info_2_banner_title' ); ?></h1>
+			<?php endif; ?>
+				<h3><?php echo get_field( 'about_info_2_hashtag' ); ?></h3>
 			</div>
-			<div class="col-12 col-md-6 info-2-image">
+			<div class="col-12 info-2-image">
 				<?php
 				echo do_shortcode( '[smartslider3 slider=3]' );
 				?>
 			</div>
-			<div class="col-12 col-md-6 info-2-desc">
+			<div class="col-12 info-2-desc">
 				<p><?php echo get_field( 'about_info_2_description' ); ?></p>
 			</div>
 		</div>
 	</div>
 
-	<div class="col-12 offset-md-2 col-md-8 about-info-3">
+	<div class="col-12 col-md-9 mx-auto about-info-3 mb-5">
 		<div class="row">
-			<div class="col-12 col-md-6 info-3-image">
+			<div class="col-12 info-3-image">
 				<div class="image-container">
 					<img src="<?php echo get_field( 'about_info_3_image' ); ?>" alt="Info Image">
 				</div>
 			</div>
-			<div class="col-12 col-md-6 info-3-desc">
+			<div class="col-12 info-3-desc">
 				<div class="i3-desc-1">
 					<p><?php echo get_field( 'about_info_3_description_1' ); ?></p>
 				</div>
@@ -54,7 +56,6 @@ get_header();
 			</div>
 		</div>
 	</div>
-
 
 </div>
 <?php
